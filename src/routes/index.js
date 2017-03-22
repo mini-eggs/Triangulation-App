6ae26d25208e20e87220ace789ea9b3775ebfc51
Evan Jones <evanjones4040@gmail.com>
2017-03-21 23:12:09 -0700

    pretty route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,18 @@
 import React from "react";
 import { Scene, Router } from "react-native-router-flux";
-import { DiscoverContainer } from "../containers/discover";
-import { VoteContainer } from "../containers/vote";
-import { AboutContainer } from "../containers/about";
-import { WorkshopContainer } from "../containers/workshop";
+import { DiscoverContainer as Discover } from "../containers/discover";
+import { VoteContainer as Vote } from "../containers/vote";
+import { AboutContainer as About } from "../containers/about";
+import { WorkshopContainer as Workshop } from "../containers/workshop";
 
 export const RoutingLayer = () => {
   return (
     <Router>
       <Scene key="root">
-        <Scene
-          type="replace"
-          hideNavBar
-          key="discover"
-          component={DiscoverContainer}
-        />
-        <Scene type="replace" hideNavBar key="vote" component={VoteContainer} />
-        <Scene hideNavBar key="workshop" component={WorkshopContainer} />
-        <Scene
-          type="replace"
-          hideNavBar
-          key="about"
-          component={AboutContainer}
-        />
+        <Scene type="replace" hideNavBar key="discover" component={Discover} />
+        <Scene type="replace" hideNavBar key="vote" component={Vote} />
+        <Scene hideNavBar key="workshop" component={Workshop} />
+        <Scene type="replace" hideNavBar key="about" component={About} />
       </Scene>
     </Router>
   );
